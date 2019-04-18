04a1742dcd062b86fdf5f01094e0e315e3ab6e5a
Cristelh <Cristelh.Miranda@gmail.com>
2019-04-18 15:41:53 -0400

    added second quiz for AT-10 PROG101

---
+++ src/main/java/CristelhMiranda/movieDB/Actor.java
@@ -0,0 +1,14 @@
+package main.java.CristelhMiranda.movieDB;
+
+import java.util.ArrayList;
+
+public class Actor {
+	private String name;
+	private ArrayList<Movie> movies;
+	
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+		
+}
